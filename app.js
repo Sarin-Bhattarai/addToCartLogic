5114ed31aec823aeab39e93730e71ac09080778e
sarin punk <sarin.bhattarai@icp.edu.np>
2022-07-25 12:17:39 +0545

    feat(add cart)

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ var app = express();
 app.use(cors({ origin: "*" }));
 app.use("/uploads", express.static("uploads"));
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- controllers/cart.js
+++ controllers/cart.js
@@ -1,4 +1,4 @@
-var Cart = require("../models/cart");
+var cartRepository = require("../repository/cart");
 var Product = require("../models/product");
 
 module.exports = {
@@ -7,7 +7,7 @@ module.exports = {
     const quantity = Number.parseInt(req.body.quantity);
 
     try {
-      let cart = await Cart.find();
+      let cart = await cartRepository.cart();
       let productDetails = await Product.findById(productId);
       if (!productDetails) {
         return res.status(500).json({
@@ -37,7 +37,56 @@ module.exports = {
         }
         //check if product exist,just add the previous quantity with the new quantity and update the total price
         else if (indexFound !== -1) {
+          cart.items[indexFound].quantity =
+            cart.items[indexFound].quantity + quantity;
+          cart.items[indexFound].total =
+            cart.items[indexFound].quantity * productDetails.price;
+          cart.items[indexFound].price = productDetails.price;
+          cart.subTotal = cart.items
+            .map((item) => item.total)
+            .reduce((acc, next) => acc + next);
         }
+        //----Check if Quantity is Greater than 0 then add item to items Array ----
+        else if (quantity > 0) {
+          cart.items.push({
+            productId: productId,
+            quantity: quantity,
+            price: productDetails.price,
+            total: parseInt(productDetails.price * quantity),
+          });
+          cart.subTotal = cart.items
+            .map((item) => item.total)
+            .reduce((acc, next) => acc + next);
+        }
+        //----if quantity of price is 0 throw the error -------
+        else {
+          return res.status(400).json({
+            status: "Invalid",
+            message: "Invalid request",
+          });
+        }
+        let data = await cart.save();
+        res.status(200).json({
+          status: "Success",
+          message: "Process successful",
+          data: data,
+        });
+      }
+      //---- if there is no user with a cart...it creates a new cart and then adds the item to the cart that has been created----
+      else {
+        const cartData = {
+          items: [
+            {
+              productId: productId,
+              quantity: quantity,
+              total: parseInt(productDetails.price * quantity),
+              price: productDetails.price,
+            },
+          ],
+          subTotal: parseInt(productDetails.price * quantity),
+        };
+        cart = await cartRepository.addItem(cartData);
+        res.json(cart);
       }
     } catch (ex) {
       return res.json({ status: "Error", message: ex.message });
